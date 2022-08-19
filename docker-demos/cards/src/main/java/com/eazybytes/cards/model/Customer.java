56d9fac4edebb7b71303ebd1181fbea311bed51d
AishwaryaaP <aishwarya.phirke17@gmail.com>
2022-08-19 09:35:21 +0530

    adding all files to git

---
+++ docker-demos/cards/src/main/java/com/eazybytes/cards/model/Customer.java
@@ -0,0 +1,21 @@
+package com.eazybytes.cards.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter @Setter @ToString
+public class Customer {
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	private int customerId;
+
+}
